f3acdad3b1910a3a93a36a3cf28c52615bf026ec
jirkajandourek <jirka.jandourek@gmail.com>
2023-02-25 00:03:20 +0100

    added our sign in component as well as firebase utils and brought in firebase libraries

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -5,6 +5,85 @@ import {
   signInWithPopup,
   GoogleAuthProvider,
 } from 'firebase/auth';
+import { getFirestore,doc,getDoc,setDoc } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAoXXOUDOCpS_-DHqtB4uoNmwKFuqZDb-w",
+  authDomain: "crwn-clothing-db-3.firebaseapp.com",
+  projectId: "crwn-clothing-db-3",
+  storageBucket: "crwn-clothing-db-3.appspot.com",
+  messagingSenderId: "439592944976",
+  appId: "1:439592944976:web:dfa6a3740fe739da731855"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: 'select_account',
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth,provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+
+  const userDocRef = doc(db,'users',userAuth.uid);
+
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot);
+  console.log(userSnapshot.exists());
+
+  if (!userSnapshot.exists()) {
+    const { displayName,email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef,{
+        displayName,
+        email,
+        createdAt
+      });
+
+    } catch (error) {
+      console.log('error creating the user',error.message);
+    }
+  }
+
+  return userDocRef;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from 'firebase/auth';
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
@@ -51,3 +130,4 @@ export const createUserDocumentFromAuth = async (userAuth) => {
 
   return userDocRef;
 };
+ */
